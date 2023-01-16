38ece6ba308ebe03a4cd87612284dfdaa4cca258
Dinozood <57855181+Dinozood@users.noreply.github.com>
2023-01-16 05:20:02 +0300

    Update main.c

--- Controller/main.c
+++ Controller/main.c
@@ -6,18 +6,18 @@ int main(void) {
 	SetupClocks();
 	SetupGPIO();
 	SetupSPI();
-	SetupTimer(1000, 30);
+	SetupTimer(1000, 2);
 
 	NVIC_EnableIRQ(TIM2_IRQn);
 	uint8_t buf[BUFFER_SIZE] = {0};
-    int kik = 0;
-    TIM2->CR1 |= 1;										//
+	uint8_t cmd = 0;
+    TIM2->CR1 |= 1;
     /* Loop forever */
 
 	for(;;){
 		SPI_Receive(&buf, BUFFER_SIZE);
-		GPIOA->ODR = buf[0];
-		GPIOA->ODR = buf[1];
+		cmd = ( ( (buf[1]&0x3) << 2) | (buf[0]&0x3) ) << 8;
+		GPIOA->ODR = cmd;
 		clearTimer();
 	}
 }
