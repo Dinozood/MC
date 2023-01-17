52fb6701b13efa32e7934c37f4db0841e41be4fa
dinozood <multiplydino@gmail.com>
2023-01-17 12:35:23 +0300

    Python sending SPI now

--- analizeVideo.py
+++ analizeVideo.py
@@ -1,103 +1,116 @@
 import cv2
 import numpy as np
+import spidev
+#//			GPIOA->ODR ^= (2 << 8); // for right 10 is backward, so 01 is forward;
+#//			GPIOA->ODR ^= (2 << 10); // same for left
 
 states = {"forward, right, left"}
 state = None
 
 
-def say_forward():
-    global state
-    if state is not "forward":
-        state = "forward"
-        print("forward")
+class Analiser:
+    def __init__(self):
+        self.spi = spidev.SpiDev()
+        self.spi.open(0, 0)
+        self.spi.max_speed_hz = 1 * (10 ** 6)
 
+        # to_send = [1, 2, 3, 5, 5, 6, 7, 8]
+        # spi.xfer(to_send)
 
-def say_left():
-    global state
-    if state is not "left":
-        state = "left"
-        print("left")
+    def say_forward(self):
+        global state
+        if state is not "forward":
+            state = "forward"
+            print("forward")
+            to_send = [1, 1]
+            self.spi.xfer(to_send)
 
+    def say_left(self):
+        global state
+        if state is not "left":
+            state = "left"
+            print("left")
+            to_send = [2, 1]
+            self.spi.xfer(to_send)
 
-def say_right():
-    global state
-    if state is not "right":
-        state = "right"
-        print("right")
+    def say_right(self):
+        global state
+        if state is not "right":
+            state = "right"
+            print("right")
+            to_send = [1, 2]
+            self.spi.xfer(to_send)
 
+    def go_forward(self):
+        self.say_forward()
 
-def go_forward():
-    say_forward()
+    def rotate_right(self, cap):
+        self.say_right()
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if ret:
+                # Display the resulting frame
+                cv2.imshow('Frame', frame)
+                cv2.waitKey(25)
+                tmp = frame[0][250:252, 0]
+                sum = tmp[0] + tmp[1]
+                if sum > 150:
+                    self.go_forward()
+                    break
 
+    def rotate_left(self, cap):
+        self.say_left()
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if ret:
+                # Display the resulting frame
+                cv2.imshow('Frame', frame)
+                cv2.waitKey(25)
+                tmp = frame[0][250:252, 0]
+                sum = int(tmp[0]) + int(tmp[1])
+                if sum > 150:
+                    self.go_forward()
+                    break
 
-def rotate_right(cap):
-    say_right()
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if ret:
-            # Display the resulting frame
-            cv2.imshow('Frame', frame)
-            cv2.waitKey(25)
-            tmp = frame[0][250:252, 0]
-            sum = tmp[0] + tmp[1]
-            if sum > 150:
-                go_forward()
-                break
-
-
-def rotate_left(cap):
-    say_left()
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if ret:
-            # Display the resulting frame
-            cv2.imshow('Frame', frame)
-            cv2.waitKey(25)
-            tmp = frame[0][250:252, 0]
-            sum = int(tmp[0]) + int(tmp[1])
-            if sum > 150:
-                go_forward()
-                break
-
-
-def start_analize(cap):
-    global state
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if ret:
-            # Display the resulting frame
-            cv2.imshow('Frame', frame)
-            tmp = frame[0][250:252, 0]
-            sum = int(tmp[0]) + int(tmp[1])
-            if sum > 150:
-                go_forward()
+    def start_analize(self, cap):
+        global state
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if ret:
+                # Display the resulting frame
+                cv2.imshow('Frame', frame)
+                tmp = frame[0][250:252, 0]
+                sum = int(tmp[0]) + int(tmp[1])
+                if sum > 150:
+                    self.go_forward()
+                else:
+                    tmp_r = frame[248:250, 499, 0]
+                    sum_r = tmp_r[0] + tmp_r[1]
+                    if sum_r > 150:
+                        self.rotate_right(cap)
+                    tmp_l = frame[248:250, 0, 0]
+                    sum_l = tmp_l[0] + tmp_l[1]
+                    if sum_l > 150:
+                        self.rotate_left(cap)
+                if state is not "forward":
+                    state = "forward"
+                    print("forward")
+                # Press Q on keyboard to  exit
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    break
             else:
-                tmp_r = frame[248:250, 499, 0]
-                sum_r = tmp_r[0] + tmp_r[1]
-                if sum_r > 150:
-                    rotate_right(cap)
-                tmp_l = frame[248:250, 0, 0]
-                sum_l = tmp_l[0] + tmp_l[1]
-                if sum_l > 150:
-                    rotate_left(cap)
-            if state is not "forward":
-                state = "forward"
-                print("forward")
-            # Press Q on keyboard to  exit
-            if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
-        else:
-            break
-            # When everything done, release the video capture object
+                # When everything done, release the video capture object
 
 
 def main():
+    visor = Analiser()
     cap = cv2.VideoCapture("/home/dinozood/Projects/IFMO/MC/output.avi")
     if not cap.isOpened():
         print("Error opening video stream or file")
 
         # Capture frame-by-frame
-    start_analize(cap)
+    visor.start_analize(cap)
     cap.release()
 
     # Closes all the frames
